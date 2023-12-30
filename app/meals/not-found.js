c0e820474a4d6040d191f9904599b2291613f837
tiedsandi <fachransandi@gmail.com>
2023-12-30 21:25:56 +0700

    create detail meal

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className='not-found'>
+      <h1>Not Found</h1>
+      <p>Unfortanately, we could not find the requested page or meal data.</p>
+    </main>
+  );
+}
--- app/not-found.js
+++ app/not-found.js
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <main className='not-found'>
       <h1>Not Found</h1>
-      <p>Unfortunately, we could not find the requested page or</p>
+      <p>Unfortunately, we could not find the requested page or resource.</p>
     </main>
   );
 };
