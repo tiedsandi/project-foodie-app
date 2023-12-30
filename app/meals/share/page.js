e2e7b3caa216b3f689ac385fdc404d44d0712c56
tiedsandi <fachransandi@gmail.com>
2023-12-30 23:01:08 +0700

    add image picker for form share meal

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,3 +1,4 @@
+import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
@@ -33,7 +34,7 @@ export default function ShareMealPage() {
             <label htmlFor='instructions'>Instructions</label>
             <textarea id='instructions' name='instructions' rows='10' required></textarea>
           </p>
-          IMAGE PICKER
+          <ImagePicker />
           <p className={classes.actions}>
             <button type='submit'>Share Meal</button>
           </p>
